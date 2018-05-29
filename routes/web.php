a4eef619c010388268c742dcb8221f31b4360913
zhangshuo <zhangshuolove@live.com>
2018-05-29 19:32:32 +0800

    控制器

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('/', 'AuthController');
+Route::get('/', 'AuthController@index');
+Route::post('/', 'AuthController@index');
+
